3269e921ed4898504d9c8ac21864a6196383df7c
mwfraczek <mwfraczek@icloud.com>
2025-04-19 08:44:49 -0400

    i2c framework added

--- inc/stm32l496.h
+++ inc/stm32l496.h
@@ -9,10 +9,15 @@ typedef unsigned int uint32_t;
 #define GPIOB_ODR    (*(volatile uint32_t*)0x48000414) // GPIOB output data register
 #define GPIOB_IDR    (*(volatile uint32_t*)0x48000410) // GPIOB input data register
 #define GPIOB_PUPDR  (*(volatile uint32_t*)0x4800040C) // GPIOB pull up/down register
+#define GPIOB_AFRH   (*(volatile uint32_t*)0x48000424) // GPIOB alternate function hi register
+
+#define I2C_CR1      (*(volatile uint32_t*)0x40005400) // I2C1
 
 #define LED_USER      7   // LD2 on Nucleo board - Output
 #define LED_EXT       13  // D18 on Nucleo board - Output
 #define SWITCH        12  // D19 on Nucleo board - Input 
 #define LED_SWITCH    10  // D27 on Nucleo board - Output
+#define I2C1_SCL      8   // D15  
+#define I2C1_SDA      9   // D16
 
 #endif
