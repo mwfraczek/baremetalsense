a47e88d74660f422b51cb2457709472f6e4fc101
mwfraczek <mwfraczek@icloud.com>
2025-04-18 21:09:06 -0400

    additional port B GPIO functions

--- inc/stm32l496.h
+++ inc/stm32l496.h
@@ -3,22 +3,16 @@
 
 typedef unsigned int uint32_t;
 
-#define RCC_AHB2ENR  (*(volatile uint32_t*)0x4002104C) // LED clock enable
-#define RCC_APB1ENR1 (*(volatile uint32_t*)0x40021058) // GPS clock enable
+#define RCC_AHB2ENR  (*(volatile uint32_t*)0x4002104C) // Port B clock enable
 
 #define GPIOB_MODER  (*(volatile uint32_t*)0x48000400) // GPIOB base address and mode register
 #define GPIOB_ODR    (*(volatile uint32_t*)0x48000414) // GPIOB output data register
+#define GPIOB_IDR    (*(volatile uint32_t*)0x48000410) // GPIOB input data register
+#define GPIOB_PUPDR  (*(volatile uint32_t*)0x4800040C) // GPIOB pull up/down register
 
-#define GPIOD_MODER  (*(volatile uint32_t*)0x48000C00) // GPIOD base address and mode register
-#define GPIOD_AFRH   (*(volatile uint32_t*)0x48000C24) // GPIOD alternate function register
-
-#define USART3_CR1   (*(volatile uint32_t*)0x40004800)
-#define USART3_BRR   (*(volatile uint32_t*)0x4000480C)
-#define USART3_RDR   (*(volatile uint32_t*)0x40004824)
-#define USART3_ISR   (*(volatile uint32_t*)0x4000481C)
-
-#define LED_PIN7      7   // LD2 on Nucleo board
-#define LED_PIN14     14  // LD3 on Nucleo board
-#define USART3_RXPIN  9
+#define LED_USER      7   // LD2 on Nucleo board - Output
+#define LED_EXT       13  // D18 on Nucleo board - Output
+#define SWITCH        12  // D19 on Nucleo board - Input 
+#define LED_SWITCH    10  // D27 on Nucleo board - Output
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,36 +1,41 @@
 #include "stm32l496.h"
 
+void delay_ms(uint32_t ms) { 
+	for (uint32_t i = 0; i < ms * 4000; i++);
+	}
+
 int main(void) {
 RCC_AHB2ENR |= (1 << 1);   // Enable GPIOB clock
-RCC_APB1ENR1 |= (1 << 18); // Enable USART3 clock 
 
-GPIOD_MODER &= ~(0x3 << (USART3_RXPIN * 2)); // clear rx bit 
-GPIOD_MODER |= (0x2 << (USART3_RXPIN * 2));  // set to alternate function mode
-GPIOD_AFRH &= ~(0xF << 0);		     // tx clear
-GPIOD_AFRH |= (7 << 0);			     // tx AF7 
-GPIOD_AFRH &= ~(0xF << 4);		     // rx clear
-GPIOD_AFRH |= (7 << 4);			     // rx AF7
+GPIOB_MODER &= ~(0x3 << (LED_USER * 2));     // Clear bits
+GPIOB_MODER |= (0x1 << (LED_USER * 2));      // Set PB7 to output mode
 
-USART3_CR1 = (1 << 2) | (1 << 0);            // RX, TX, USART enable
-USART3_BRR = 0x683;                          // 9600 baud rate @ 16 Mhz
+GPIOB_MODER &= ~(0x3 << (LED_EXT * 2));  
+GPIOB_MODER |= (0x1 << (LED_EXT * 2));       // Set PB13 to output mode
 
-GPIOB_MODER &= ~(0x3 << (LED_PIN7 * 2));  // Clear bits for PB7
-GPIOB_MODER |= (0x1 << (LED_PIN7 * 2));   // Set PB7 to output mode
-GPIOB_MODER &= ~(0x3 << (LED_PIN14 * 2)); // Reset state - clears bits for PB14
-GPIOB_MODER |= (0x1 << (LED_PIN14 * 2));  // Set PB14 to output mode
+GPIOB_MODER &= ~(0x3 << (SWITCH * 2));
+GPIOB_MODER |= (0x0 << (SWITCH * 2));        // Set PB12 to output mode
+
+GPIOB_PUPDR &= ~(0x3 << (LED_SWITCH * 2));
+GPIOB_PUPDR |= (0x2 << (LED_SWITCH * 2));    // Set PB10 pull down resistor
+
+GPIOB_MODER &= ~(0x3 << (LED_SWITCH * 2));
+GPIOB_MODER |= (0x1 << (LED_SWITCH * 2));    // Set PB10 - output mode
+
 
 while (1) {
-if (USART3_ISR & (1 << 5)) {           // RXNE
-        char c = USART3_RDR;           // Read GPS byte
-        GPIOB_ODR ^= (1 << LED_PIN7);  // Toggle LED
-	char gps_data[80];
-	static int i = 0;
-	gps_data[i] = c;
-	i++;
-}	
-
-else if (USART3_ISR & (0 << 5)) {
-	GPIOB_ODR ^= (1 << LED_PIN14); // Toggle LED
-}
-}
+        GPIOB_ODR |= (1 << LED_USER);  // Toggle Blue LED on Nucleo 
+	delay_ms(10);
+	GPIOB_ODR &= (0 << LED_USER);
+	delay_ms(10);
+	GPIOB_ODR |= (1 << LED_EXT); // Toggle external breadboard LED 
+        delay_ms(10);
+        GPIOB_ODR &= (0 << LED_EXT);
+        delay_ms(10);
+	if (GPIOB_IDR & (1 << SWITCH)) {
+			GPIOB_ODR |= (1 << LED_SWITCH); // Toggle 2nd breadboard LED
+        		delay_ms(10);
+			GPIOB_ODR &= (0 << LED_SWITCH);
+			}
+	}
 }
