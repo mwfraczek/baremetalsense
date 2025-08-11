46fe403e9977e6231150116ea07e5dfe71f88d99
mwfraczek <mwfraczek@icloud.com>
2025-08-10 22:17:15 -0400

    README addition

--- src/peripherals.c
+++ src/peripherals.c
@@ -2,21 +2,25 @@
 #include "peripherals.h"
 
 void rcc_enable(void) {
-	RCC_AHB2ENR |= (1 << 1);  // Enable GPIOB clock (for I2C1 pins PB8/PB9)
-	RCC_APB1ENR1 |= (1 << 21);  // Enable I2C1 clock
+	RCC_AHB2ENR   |= (0x1 << 1);      // Enable GPIOB clock
+	RCC_APB1ENR1  |= (0x1 << 21);     // Enable I2C1 clock
+	RCC_CR        |= (0x1 << 8);      // Enable HSI16 clock
+	while (!(RCC_CR & (0x1 << 10)));  // Wait for HSIRDY
+	RCC_CCIPR     |=  (0x2 << 12);    // I2C1SEL  (HSI is I2C1 clock source)
 }
 
 void gpio_mode(void) {
-	GPIOB_MODER &= ~(3 << 6);  // Clear PB3 bits
-	GPIOB_MODER |= (1 << 6);  // Set PB3 to output mode
+	GPIOB_MODER   &= ~(0x3 << 14);   // Clear bits
+	GPIOB_MODER   |=  (0x1 << 14);   // Set PB7 to output
 }
 
 void delay_ms(uint32_t ms) {
-	for (uint32_t i = 0; i < ms * 4000; i++) {  // Rough delay, tune for your clock
-		__asm__("nop");
-	}
+	for (uint32_t i = 0; i < ms * 4000; i++);   // Rough delay, tune for your clock
 }
 
 void led_toggle(void) {
-	GPIOB_ODR ^= (1 << 3);  // Toggle PB3 LED
+	GPIOB_ODR |= (1 << 7);  // Toggle PB3 LED
+	delay_ms(50);
+	GPIOB_ODR &= ~(1 << 7);  // Toggle PB3 LED
+	delay_ms(50);
 }
