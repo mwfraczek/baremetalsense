d52e3e2656dfa0aa5d7140a2d79c11cfadd6fc08
fraxek <mwfraczek@icloud.com>
2025-10-15 16:28:26 -0400

    update bmp390 driver to align with new i2c functions

--- drivers/bmp390.c
+++ drivers/bmp390.c
@@ -1,41 +1,24 @@
+// BMP390 Device Driver Library 
+
 #include "stm32l476.h"
 #include "i2c.h"
 #include "peripherals.h"
 #include "bmp390.h"
 
-BMP_Error bmp390_chip_id(uint8_t *id){
-	if (i2c1_write_reg(BMP_SLAVE_ADDR, BMP_CHIPID_ADDR) != 0)
-		return BMP_NACK;
-	if (i2c1_read(BMP_SLAVE_ADDR, id, 1) != 0)
-		return BMP_NACK;
-	return BMP_OK;
+// BMP390 Configuration Function 
+int bmp390_setup(void) {
+	i2c1_write(BMP_SLAVE_ADDR, BMP_PWRCTRL_ADDR, BMP_PWRCTRL, 1);
+	i2c1_write(BMP_SLAVE_ADDR, BMP_OSR_ADDR, BMP_OSR, 1);
+	i2c1_write(BMP_SLAVE_ADDR, BMP_ODR_ADDR, BMP_ODR, 1);
 }
 
-BMP_Error bmp390_setup(uint8_t pressure_en, uint8_t temp_en, uint8_t bmp_mode, uint8_t bmp_odr){
-	uint8_t pwr_ctrl = (pressure_en << 0) | (temp_en << 1) | (bmp_mode << 4);
-	if (i2c1_write(BMP_SLAVE_ADDR, BMP_PWR_CTRL_ADDR, pwr_ctrl) != 0)
-		return BMP_NACK;
-	if (i2c1_write(BMP_SLAVE_ADDR, BMP_ODR_ADDR, bmp_odr) != 0)
-		return BMP_NACK;
-	if (i2c1_write(BMP_SLAVE_ADDR, BMP_OSR_ADDR, 0x03) != 0)
-		return BMP_NACK;
-	return BMP_OK;
+int bmp390_chipid(void) {
+	uint8_t chipid; 
+	i2c1_read(BMP_SLAVE_ADDR, BMP_CHIPID_ADDR, &chipid); 
 }
 
-BMP_Error bmp390_status_check(uint8_t *bmp_status){
-	if (i2c1_write_reg(BMP_SLAVE_ADDR, BMP_STATUS_ADDR) != 0)
-		return BMP_NACK;
-	if (i2c1_read(BMP_SLAVE_ADDR, bmp_status, 1) != 0)
-		return BMP_NACK;
-	return BMP_OK;
-}
-
-BMP_Error bmp390_read_data(uint8_t *bmp_data){
-	if (i2c1_write_reg(BMP_SLAVE_ADDR, BMP_DATA_ADDR) != 0)
-		return BMP_NACK;
-	i2c1_reset();
-	if (i2c1_read(BMP_SLAVE_ADDR, bmp_data, 6) != 0)
-		return BMP_NACK;
-	return BMP_OK;
+int bmp390_status(void) {
+        uint8_t status; 
+        i2c1_read(BMP_SLAVE_ADDR, BMP_STATUS_ADDR, &status); 
 }
 
