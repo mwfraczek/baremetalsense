a2384b581ea6c8e2c05aa6e7da14fcca3165a2a2
fraxek <mwfraczek@icloud.com>
2025-10-15 16:38:15 -0400

    set HSI as SYSCLK source

--- src/system.c
+++ src/system.c
@@ -1,11 +1,13 @@
 #include "stm32l476.h"
 
+// Enable HSI16 (16 MHz) and switch SYSCLK source to HSI16
 void SystemInit(void) {
-    // Enable HSI16 (16 MHz) for STM32L476RG
-    RCC_CR |= (1 << 8); // RCC_CR_HSI16ON
-    while (!(RCC_CR & (1 << 10))); // Wait for RCC_CR_HSI16RDYF
+    RCC_CR |= (1 << 8); 	    // HSI16 clock enable
+    while (!(RCC_CR & (1 << 10)));  // Wait for stable oscillator flag
+    RCC_CFGR |= (1 << 0); 	    // Select SYSCLK source as HSI16
+    while (!(RCC_CFGR & (1 << 2))); // Check for correct clock source
 }
 
+// Empty - no C runtime initialization needed
 void _init(void) {
-    // Empty, as no C runtime initialization is needed
 }
